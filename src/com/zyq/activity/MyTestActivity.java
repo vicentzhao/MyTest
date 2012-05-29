ebd2b8eeda21e635076627bdda7e45063b5c341d
zhaoyiqun <nice_zhyq@163.com>
2012-05-29 14:23:40 +0800

    init

---
+++ src/com/zyq/activity/MyTestActivity.java
@@ -0,0 +1,13 @@
+package com.zyq.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
